db1f71243f2fd9bc53f2b5ae9786e4e5c926e87f
Alertinime <40435135+Alertinime@users.noreply.github.com>
2023-05-09 14:51:50 +0200

    opti

--- Source/Demo Gamplay combat/Game.py
+++ Source/Demo Gamplay combat/Game.py
@@ -19,27 +19,10 @@ class Game:
         self.descrp = Descripteur(Wind)
 
     def event(self):
-        left,mid,right = pygame.mouse.get_pressed()
-        if right:
-            if self.act_sect.entity != "" or self.selected.entity != "":
-                if not self.select:
-                    
-                    self.selected = self.act_sect
-                    if not self.dc:
-                        self.select = True
-                        print(self.select)
-                    else:
-                        self.dc = False
-                        print(self.select)
-                        self.selected = self.map.matrice[0][0]
-                pygame.time.delay(70)
-                if self.select and self.act_sect != self.selected and self.selected != self.map.matrice[0][0]:
-                    self.select = False
-                    self.dc = True
-                    self.selected.swapEnt(self.act_sect)
-                    self.selected.render(1)
-                    self.selected = self.map.matrice[0][0]
-                
+        self.__checkmousemoov()
+        
+        
+    def __checkmousemoov(self):
         if self.mouse_pos != pygame.mouse.get_pos():
             x,y = self.mouse_pos
             cx = 0
