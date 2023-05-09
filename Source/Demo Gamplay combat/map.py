ef8b1023b4243e5b1bb5480ba2e70053386a8daa
Alertinime <40435135+Alertinime@users.noreply.github.com>
2023-05-09 15:31:44 +0200

    Ajout des commentaires sur la class Map

--- Source/Demo Gamplay combat/map.py
+++ Source/Demo Gamplay combat/map.py
@@ -10,16 +10,21 @@ class Map:
     def __gen_matr(self):
  
         row,col = self.res
+        #calcul du nombre de section a faire
         nombreX = int(row/30)
-        nombreY = int(col/30)       
+        nombreY = int(col/30)
+        #Recuperation de lq mqtrice csv
         mp_matrx = self.__matrx_csv(nombreX,nombreY)
+        #creqtion d'une martrice de taille Nombre X et de largeur Nombre Y
         matrx = [[0 for i in range(nombreX)] for j in range(nombreY)]
         x = 0
         y = 0
         xm = 0
         ym = 0
+        
         for nx in range(nombreX):
             for ny in range(nombreY):
+                #Pour chaque case de la matrice, creation d'une section et attitrage de celle si
                 type = mp_matrx[ny][nx]
 
                 matrx[ny][nx] = Section(x,y,self.win,type,0)
@@ -36,17 +41,18 @@ class Map:
         pygame.display.flip() 
         return matrx
     def __matrx_csv(self,nx,ny):
+        #creation d'une matric de nx X ny
         map_matrx =[[0 for i in range(nx)] for j in range(ny)]
-        with open("../../Data/maps/lv1.csv", "r") as file:
+        with open("../../Data/maps/lv1.csv", "r") as file: #Ouverture du csv
             mfile = csv.reader(file)
             y = 0
             xu = 0
             for lines in mfile:
                 for x in lines[0:nx]:
-                    map_matrx[y][xu] = x
+                    map_matrx[y][xu] = x #inscription des informations
                     xu +=1
                 xu = 0
                 y += 1
                 if y>=ny:
-                    break
+                    break # finis
         return map_matrx
