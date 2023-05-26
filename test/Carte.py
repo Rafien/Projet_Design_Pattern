55c90041b688d46e69f7ca723f2acd4411e80700
Raf <raphael.choquet@gmail.com>
2023-05-26 14:45:10 +0200

    Ajout Lecture de case

--- test/Carte.py
+++ test/Carte.py
@@ -42,3 +42,10 @@ class Carte:
                     if self.carte[y][x] == "X":
                         self.carte[y][x] = ressource
                         break
+    
+    def lire_case(self, x , y):
+        print("Vous êtes sur la case", x-1, y-1)
+        print("Cette case contient", self.carte[y-1][x-1])
+        for i in range(len(ressources)):
+            if self.carte[y-1][x-1] == ressources[i]:
+                print("Vous avez trouvé "+ ressources[i] +"!")
--- test/main.py
+++ test/main.py
@@ -1,7 +1,13 @@
 from Carte import Carte
 
-carte = Carte()
-carte.creer_carte()
-carte.afficher_carte()
-carte.ajouter_ressources()
-carte.afficher_carte()
+
+def test_carte():
+    carte = Carte()
+    carte.creer_carte()
+    carte.afficher_carte()
+    carte.ajouter_ressources()
+    carte.afficher_carte()
+    carte.lire_case(5,5)
+
+
+test_carte()
